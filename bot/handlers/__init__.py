7e7cfee81e0187eb010c709b3384e6b1a7817d81
Nikolay1123770 <131786139+Nikolay1123770@users.noreply.github.com>
2026-01-20 09:57:33 +0300

    Delete bot/handlers directory

--- bot/handlers/__init__.py
+++
@@ -1,9 +0,0 @@
-from bot.handlers.start import router as start_router
-from bot.handlers.purchase import router as purchase_router
-from bot.handlers.support import router as support_router
-
-routers = [
-    start_router,
-    purchase_router,
-    support_router,
-]
